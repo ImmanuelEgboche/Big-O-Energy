572d079d2afbb6daa8fafc6b2b04da1b1d6250b6
Immanuel Egboche <immanuelegboche@Immanuels-Mac-mini.local>
2025-07-02 23:24:29 +0100

    addign

--- BigOEnergy/BinarySearch/Searcha2DMarix.py
+++ BigOEnergy/BinarySearch/Searcha2DMarix.py
@@ -27,6 +27,8 @@ OR
 
 Treat the matrix as a virtual sorted array of length m*n as its a 2d array
 Use binary search with left as 0 and teh right as m*n -1 that would now be the last point
+
+The main part is row calculation and column caluclation 
 - 
 """
 
@@ -37,3 +39,33 @@ time complexity of this would be O( m * logn)  m being the arrays we have in the
 """
 
 # Code
+
+def searchMatrix(matrix, target):
+    m, n = len(matrix), len(matrix[0])
+    left, right = 0, m * n - 1
+
+    while left <= right:
+        mid = (left+ right) // 2
+
+        # convert to a 2D array 
+
+        row = mid // n
+        col = mid % n 
+
+        mid_value = matrix[row][col]
+
+        if mid_value == target:
+            return True
+        elif mid_value < target:
+            left = mid + 1
+        else: 
+            right = mid - 1
+    
+    return False 
+
+"""
+The beauty is that we get O(log(m×n)) time complexity because we're doing binary search on the 
+conceptually flattened array, but we never actually create that flattened array - 
+we just use math to map between the 1D search space and 2D coordinates.
+
+"""
